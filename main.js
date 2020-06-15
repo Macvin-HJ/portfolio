a5d6af520d509ad02d981cd810ba5c7b58b84201
SrcCoderH <64532265+SrcCoderH@users.noreply.github.com>
2020-06-15 11:20:28 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.addEventListener("scroll", () => {
   }
 });
 
-//Handle scrolling when tapping on the navbar menu
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
@@ -19,7 +19,14 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 // Handle click on "contact me" button on home
@@ -28,7 +35,7 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-//Make home slowly fade to transparent as the window scrolls down
+// Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
